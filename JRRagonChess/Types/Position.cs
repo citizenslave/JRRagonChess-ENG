1ea0b152782c3a2e50893897bd4a7243dec45213
Will McVay <mcvay.will@gmail.com>
2023-10-28 16:35:02 -0400

    cleanup types

--- JRRagonChess/Types/Position.cs
+++ JRRagonChess/Types/Position.cs
@@ -1,29 +1,75 @@
 using JRRagonGames.JRRagonChess.BoardState;
 
 namespace JRRagonGames.JRRagonChess.Types {
-    public struct Position {
-        public int rank, file;
+    public readonly struct Position {
+        #region Data/Constructor
+        public readonly int rank, file;
+        public Position(int _rank, int _file) { rank = _rank; file = _file; }
+        #endregion
 
-        public readonly int Index {
-            get {
-                return GetIndex(rank, file);
-            }
-        }
 
-        public readonly Position OffsetByIndex(int offsetIndex) => GetPositionFromIndex(Index + offsetIndex);
 
+        #region Instance Accessors
+        public readonly int Index => GetIndex(rank, file);
         public override readonly string ToString() => GetNameFromPosition(file, rank);
 
-        public static int GetIndex(string squareName) => GetPositionFromName(squareName).Index;
-        public static int GetIndex(int rank, int file) => rank * Board.Constants.FileCount + file;
-        public static int GetFileFromName(string name) => "abcdefgh".IndexOf(char.ToLower(name[0]));
-        public static int GetRankFromName(string name) => (int)(char.GetNumericValue(name[1]) - 1);
+
+
+        public readonly Position OffsetByIndex(int offsetIndex) => GetPositionFromIndex(Index + offsetIndex);
+        #endregion
+
+
+
+
+
+
+        #region Static Utilities
+
+
+
+        #region Component Extraction
+        #region Components from Index
         public static int GetFileFromIndex(int index) => index % Board.Constants.FileCount;
         public static int GetRankFromIndex(int index) => index / Board.Constants.FileCount;
+        #endregion
+
+
+
+        #region Components from String
+        public static int GetFileFromName(string name) => "abcdefgh".IndexOf(char.ToLower(name[0]));
+        public static int GetRankFromName(string name) => (int)(char.GetNumericValue(name[1]) - 1);
+        #endregion
+        #endregion
+
+
+
+        #region Conversion Utilities
+        #region Index Conversion
+        public static int GetIndex(string squareName) => GetPositionFromName(squareName).Index;
+        public static int GetIndex(int rank, int file) => rank * Board.Constants.FileCount + file;
+        #endregion
+
+
+
+        #region Name Conversion
         public static string GetNameFromPosition(int file, int rank) => $"{"abcdefgh"[file]}{rank + 1}";
+        #endregion
+        #endregion
+
+
+
+        #region Factories
         public static Position GetPositionFromName(string name) =>
-            new Position() { rank = GetRankFromName(name), file = GetFileFromName(name) };
+            new Position(GetRankFromName(name), GetFileFromName(name));
         public static Position GetPositionFromIndex(int squareIndex) =>
-            new Position() { rank = GetRankFromIndex(squareIndex), file = GetFileFromIndex(squareIndex) };
+            new Position(GetRankFromIndex(squareIndex), GetFileFromIndex(squareIndex));
+        #endregion
+
+
+
+        #endregion
+
+
+
     }
 }

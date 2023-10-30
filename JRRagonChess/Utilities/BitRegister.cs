0ee2e8d4c4171b171a0841b354dfb851227ca43c
Will McVay <mcvay.will@gmail.com>
2023-10-30 12:54:33 -0400

    BitRegister Class

---
+++ JRRagonChess/Utilities/BitRegister.cs
@@ -0,0 +1,14 @@
+﻿namespace JRRagonGames.Utilities {
+    public class BitRegister {
+        private int bitData;
+
+        public BitRegister(int _bitData, uint size = 32) { bitData = _bitData; }
+
+        public int this[int mask] {
+            get => (bitData & mask) >> BitUtilities.FindLSB(mask);
+            set => bitData = (bitData & ~mask) | ((value << BitUtilities.FindLSB(mask)) & mask);
+        }
+
+        public int BitData { get => bitData; set => bitData = value; }
+    }
+}

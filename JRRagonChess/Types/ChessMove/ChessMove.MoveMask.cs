9be6cdb7bd4eab2d3495f00007da36dbf7ed722f
Will McVay <mcvay.will@gmail.com>
2023-10-30 21:46:55 -0400

    ChessMove cleanup

---
+++ JRRagonChess/Types/ChessMove/ChessMove.MoveMask.cs
@@ -0,0 +1,9 @@
+﻿namespace JRRagonGames.JRRagonChess.Types {
+    public readonly partial struct ChessMove {
+        private static class MoveMask {
+            public const int StartIndexMask = 0b111111 << MoveOffset.StartIndexOffset;
+            public const int EndIndexMask = 0b111111 << MoveOffset.EndIndexOffset;
+            public const int FlagMask = 0b1111 << MoveOffset.FlagIndexOffset;
+        }
+    }
+}
---
+++ JRRagonChess/Types/ChessMove/ChessMove.MoveOffset.cs
@@ -0,0 +1,9 @@
+﻿namespace JRRagonGames.JRRagonChess.Types {
+    public readonly partial struct ChessMove {
+        private static class MoveOffset {
+            public const int StartIndexOffset = 0x0;
+            public const int EndIndexOffset = 0x6;
+            public const int FlagIndexOffset = 0xC;
+        }
+    }
+}

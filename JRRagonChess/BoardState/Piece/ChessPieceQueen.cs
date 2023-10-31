3c5fef6cf609917578416fd71c25e41363f533b4
Will McVay <mcvay.will@gmail.com>
2023-10-31 16:42:54 -0400

    Queen GetSlidingMoves

--- JRRagonChess/BoardState/Piece/ChessPieceQueen.cs
+++ JRRagonChess/BoardState/Piece/ChessPieceQueen.cs
@@ -11,25 +11,10 @@ namespace JRRagonGames.JRRagonChess.BoardState.Piece {
         public ChessPieceQueen(int team, Position position)
             : base(ChessPieceQueenId, team, position) { }
 
-        protected override List<ChessMove> GetPseudoLegalMovesForPiece(Board currentBoardState) {
-            List<ChessMove> moves = new List<ChessMove>();
+        protected override List<ChessMove> GetPseudoLegalMovesForPiece(Board currentBoardState) =>
+            GetSlidingMoves(currentBoardState, moveOffsets);
 
-            foreach (int moveOffset in moveOffsets) {
-                for (int searchIndex = piecePosition.Index; IsValidSquare(searchIndex, moveOffset); searchIndex += moveOffset) {
-                    int targetIndex = searchIndex + moveOffset,
-                        pieceNibbleAtTarget = currentBoardState[targetIndex];
 
-                    bool targetingPiece = pieceNibbleAtTarget != ChessPieceNone,
-                        targetingOpponent = targetingPiece && GetTeamFromNibble(pieceNibbleAtTarget) != chessTeam;
-
-                    if (!targetingPiece || targetingOpponent)
-                        moves.Add(new ChessMove(piecePosition, Position.GetPositionFromIndex(targetIndex)));
-                    if (targetingPiece) break;
-                }
-            }
-
-            return moves;
-        }
 
         protected override bool IsMoveValid(ChessMove move, Board currentBoardState) {
             if (!base.IsMoveValid(move, currentBoardState)) return false;

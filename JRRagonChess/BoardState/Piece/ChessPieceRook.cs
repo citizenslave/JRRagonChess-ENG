00607d0eb27563de41627b8bb12f78d5bb6f35c8
Will McVay <mcvay.will@gmail.com>
2023-10-31 17:03:50 -0400

    Rook SlidingOffsetSelector validation

--- JRRagonChess/BoardState/Piece/ChessPieceRook.cs
+++ JRRagonChess/BoardState/Piece/ChessPieceRook.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 using JRRagonGames.JRRagonChess.Types;
 
@@ -16,14 +17,8 @@ namespace JRRagonGames.JRRagonChess.BoardState.Piece {
 
 
 
-        protected override bool IsMoveValid(ChessMove move, Board currentBoardState) {
-            if (!base.IsMoveValid(move, currentBoardState)) return false;
-
-            if (new List<int>(moveOffsets[4..]).FindIndex(
-                offset => (move.EndPosition.Index - move.StartPosition.Index) % offset == 0
-            ) == -1) return false;
-
-            return true;
-        }
+        protected override bool IsMoveValid(ChessMove move, Board currentBoardState) =>
+            base.IsMoveValid(move, currentBoardState) &&
+            (new List<int>(moveOffsets[4..]).FindIndex(SlidingOffsetSelector(move)) != -1);
     }
 }

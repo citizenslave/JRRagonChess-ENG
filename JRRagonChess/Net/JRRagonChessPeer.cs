cd7d76e39c8d58916afd06b4d818ec2ce0535fe9
Will McVay <mcvay.will@gmail.com>
2023-11-21 17:10:49 -0500

    JRRagonChessPeer

---
+++ JRRagonChess/Net/JRRagonChessPeer.cs
@@ -0,0 +1,79 @@
+﻿using JRRagonGames.JRRagonChess.Net.Types;
+using JRRagonGames.JRRagonChess.Types;
+using JRRagonGames.Utilities.Net;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JRRagonGames.JRRagonChess.Net {
+    public class JRRagonChessPeer : JRRagonUdpPeer, IJRRagonChessNet {
+        public IReadOnlyList<PendingGameRequest> WaitingGameRequests { get => _pendingGameRequests.AsReadOnly(); }
+        private List<PendingGameRequest> _pendingGameRequests = new List<PendingGameRequest>();
+        public PendingGameRequest MatchingRequest { get; private set; }
+        public ChessTeam AssignedTeam { get; private set; } = ChessTeam.NoneTeam;
+
+        public bool Connect(string url, FindGamePayload gameRequest) {
+            gamePreferences = gameRequest;
+            OnPeerConnected -= FindGame;
+            OnMessageReceived -= HandleUdpMessage;
+            
+            Connect(url, 8008);
+
+            OnPeerConnected += FindGame;
+            OnMessageReceived += HandleUdpMessage;
+
+            return IsListening;
+        }
+        private FindGamePayload gamePreferences = new FindGamePayload();
+
+        private void FindGame(byte[] obj) => Send("findGame", gamePreferences.ToString());
+
+        private void HandleUdpMessage(byte[] obj) {
+            string[] msgParts = Encoding.UTF8.GetString(obj).Split(':');
+            switch (msgParts[0]) {
+                case "findGame":
+                    MatchGame(
+                        msgParts[2],
+                        int.Parse(msgParts[3]),
+                        msgParts[4] != "0",
+                        msgParts[5] != "0"
+                    );
+                    break;
+                case "startGame":
+                    AssignedTeam = (ChessTeam)int.Parse(msgParts[3]);
+                    OnSetUciPosition?.Invoke(msgParts[2]);
+                    break;
+                case "makeMove": OnMakeMove?.Invoke(new ChessMove(ushort.Parse(msgParts[2]))); break;
+                case "rematch":
+                    pendingRematch = string.Join(':', msgParts);
+                    OnRematchRequest?.Invoke(msgParts[2], (ChessTeam)int.Parse(msgParts[3]));
+                    break;
+                default:
+                    Console.WriteLine("Unhandled UDP Message:\n" +
+                        $"{msgParts[0]}:*:{string.Join(':', msgParts[2..])}");
+                    break;
+            }
+        }
+
+        private void MatchGame(string position, int chessTeam, bool requirePosition, bool requireTeam) {
+            if (AssignedTeam == ChessTeam.NoneTeam) AssignedTeam = (ChessTeam)(chessTeam ^ 1);
+            Send("startGame", $"{position}:{(int)AssignedTeam ^ 1}");
+        }
+
+        public event Action<string>? OnSetUciPosition;
+        public event Action<ChessMove>? OnMakeMove;
+        public event Action<string, ChessTeam>? OnRematchRequest;
+        private string pendingRematch = string.Empty;
+
+        public void PostMove(ChessMove move) => Send("makeMove", move.moveData.ToString());
+        public void Rematch(string uci, ChessTeam team, bool confirm = false) => Send("rematch", $"{uci}:{(int)team}:{(confirm ? 1 : 0)}");
+        public void ConfirmRematch() {
+            if (string.IsNullOrEmpty(pendingRematch)) return;
+
+            string[] rematchParts = pendingRematch.Split(':');
+            Send("startGame", $"{rematchParts[2]}:{rematchParts[3]}");
+            OnSetUciPosition?.Invoke(rematchParts[2]);
+            AssignedTeam = (ChessTeam)(int.Parse(rematchParts[3]) ^ 1);
+        }
+    }
+}

e0c154e0a6088408b549ea44be4067b5225b15fb
Will McVay <mcvay.will@gmail.com>
2023-11-21 17:09:47 -0500

    Add shared interface

---
+++ JRRagonChess/Net/IJRRagonChessNet.cs
@@ -0,0 +1,12 @@
+﻿using JRRagonGames.JRRagonChess.Types;
+
+namespace JRRagonGames.JRRagonChess.Net {
+    public interface IJRRagonChessNet {
+        public ChessTeam AssignedTeam { get; }
+        public bool IsListening { get; }
+
+        public void PostMove(ChessMove move);
+        public void Rematch(string uci, ChessTeam team, bool confirm = false);
+        public void ConfirmRematch();
+    }
+}

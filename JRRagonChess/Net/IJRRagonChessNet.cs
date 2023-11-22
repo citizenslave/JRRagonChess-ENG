3e470f79bd2839983d5ebb930dccc1f57f3d4355
Will McVay <mcvay.will@gmail.com>
2023-11-21 22:27:12 -0500

    extend interface

--- JRRagonChess/Net/IJRRagonChessNet.cs
+++ JRRagonChess/Net/IJRRagonChessNet.cs
@@ -1,10 +1,17 @@
 ﻿using JRRagonGames.JRRagonChess.Types;
+using System;
 
 namespace JRRagonGames.JRRagonChess.Net {
     public interface IJRRagonChessNet {
         public ChessTeam AssignedTeam { get; }
         public bool IsListening { get; }
 
+        public event Action<string>? OnSetUciPosition;
+        public event Action<ChessMove>? OnMakeMove;
+        public event Action<string, ChessTeam>? OnRematchRequest;
+        public event Action OnDisconnected;
+
+        public void Disconnect();
         public void PostMove(ChessMove move);
         public void Rematch(string uci, ChessTeam team, bool confirm = false);
         public void ConfirmRematch();
